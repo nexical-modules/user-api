430643c9f96fff58469ab3999980611f1ccfdcde
Adrian Webb <adrian@webb.sh>
2026-02-07 16:19:27 -0500

    Adding auth packages and Astro configurations.

--- module.config.mjs
+++ module.config.mjs
@@ -1,9 +1,14 @@
+import auth from 'auth-astro';
+
+
 export default {
   type: 'feature',
   order: 50,
+  integrations: [auth()],
   vite: {
     optimizeDeps: {
       include: ['bcryptjs'],
     },
   },
 };
+
