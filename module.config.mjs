633065c15f56430fcafbd09dcce0c29a973fb0b5
Adrian Webb <adrian@webb.sh>
2026-02-09 18:33:53 -0500

    Disable automatic injection for auth in the module configuration.

--- module.config.mjs
+++ module.config.mjs
@@ -4,7 +4,7 @@ import auth from 'auth-astro';
 export default {
   type: 'feature',
   order: 50,
-  integrations: [auth({ configFile: './modules/user-api/auth.config' })],
+  integrations: [auth({ configFile: './modules/user-api/auth.config', injectEndpoints: false })],
   vite: {
     optimizeDeps: {
       include: ['bcryptjs'],
