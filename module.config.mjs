27c1901f811f9ef011a93edec88bd275f8979bb2
Adrian Webb <adrian@webb.sh>
2026-02-28 18:34:47 -0500

    Removing the auth integration configurations in th module configuration.

--- module.config.mjs
+++ module.config.mjs
@@ -1,9 +1,6 @@
-import auth from 'auth-astro';
-
 export default {
   type: 'feature',
   order: 50,
-  integrations: [auth({ configFile: './modules/user-api/auth.config', injectEndpoints: false })],
   vite: {
     optimizeDeps: {
       include: ['bcryptjs'],
