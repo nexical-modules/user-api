afe90403cae7232d566a38d28e74928aaef543f0
Adrian Webb <adrian@webb.sh>
2026-03-23 15:34:19 -0400

    Updates to API route unit tests.

--- tests/unit/pages/api/user/[id].test.ts
+++ tests/unit/pages/api/user/[id].test.ts
@@ -15,7 +15,7 @@ describe('User API - GET ../../../../../src/pages/api/user/[id]', () => {
 
   it('should call UserService and return success', async () => {
     const query = ['GET', 'DELETE'].includes('GET'.toUpperCase())
-      ? `?username=${encodeURIComponent(String('test'))}&email=${encodeURIComponent(String('test@example.com'))}&password=${encodeURIComponent(String('test'))}&passwordUpdatedAt=${encodeURIComponent(String(new Date().toISOString()))}&emailVerified=${encodeURIComponent(String(new Date().toISOString()))}&name=${encodeURIComponent(String('test'))}&image=${encodeURIComponent(String('test'))}&role=${encodeURIComponent(String('USER_ADMIN'))}&status=${encodeURIComponent(String('ACTIVE'))}`
+      ? `?username=${encodeURIComponent(String('test'))}&email=${encodeURIComponent(String('test@example.com'))}&passwordUpdatedAt=${encodeURIComponent(String(new Date().toISOString()))}&emailVerified=${encodeURIComponent(String(new Date().toISOString()))}&name=${encodeURIComponent(String('test'))}&image=${encodeURIComponent(String('test'))}&role=${encodeURIComponent(String('USER_ADMIN'))}&status=${encodeURIComponent(String('ACTIVE'))}`
       : '';
     const fullUrl = 'http://localhost/api/test' + query;
 
@@ -60,7 +60,7 @@ describe('User API - GET ../../../../../src/pages/api/user/[id]', () => {
 
   it('should return 400 when invalid input is provided (scaffold)', async () => {
     const query = ['GET', 'DELETE'].includes('GET'.toUpperCase())
-      ? `?username=${encodeURIComponent(String('test'))}&email=${encodeURIComponent(String('test@example.com'))}&password=${encodeURIComponent(String('test'))}&passwordUpdatedAt=${encodeURIComponent(String(new Date().toISOString()))}&emailVerified=${encodeURIComponent(String(new Date().toISOString()))}&name=${encodeURIComponent(String('test'))}&image=${encodeURIComponent(String('test'))}&role=${encodeURIComponent(String('USER_ADMIN'))}&status=${encodeURIComponent(String('ACTIVE'))}`
+      ? `?username=${encodeURIComponent(String('test'))}&email=${encodeURIComponent(String('test@example.com'))}&passwordUpdatedAt=${encodeURIComponent(String(new Date().toISOString()))}&emailVerified=${encodeURIComponent(String(new Date().toISOString()))}&name=${encodeURIComponent(String('test'))}&image=${encodeURIComponent(String('test'))}&role=${encodeURIComponent(String('USER_ADMIN'))}&status=${encodeURIComponent(String('ACTIVE'))}`
       : '';
     const fullUrl = 'http://localhost/api/test' + query;
 
@@ -86,7 +86,7 @@ describe('User API - GET ../../../../../src/pages/api/user/[id]', () => {
 
   it('should return 500 when action fails', async () => {
     const query = ['GET', 'DELETE'].includes('GET'.toUpperCase())
-      ? `?username=${encodeURIComponent(String('test'))}&email=${encodeURIComponent(String('test@example.com'))}&password=${encodeURIComponent(String('test'))}&passwordUpdatedAt=${encodeURIComponent(String(new Date().toISOString()))}&emailVerified=${encodeURIComponent(String(new Date().toISOString()))}&name=${encodeURIComponent(String('test'))}&image=${encodeURIComponent(String('test'))}&role=${encodeURIComponent(String('USER_ADMIN'))}&status=${encodeURIComponent(String('ACTIVE'))}`
+      ? `?username=${encodeURIComponent(String('test'))}&email=${encodeURIComponent(String('test@example.com'))}&passwordUpdatedAt=${encodeURIComponent(String(new Date().toISOString()))}&emailVerified=${encodeURIComponent(String(new Date().toISOString()))}&name=${encodeURIComponent(String('test'))}&image=${encodeURIComponent(String('test'))}&role=${encodeURIComponent(String('USER_ADMIN'))}&status=${encodeURIComponent(String('ACTIVE'))}`
       : '';
     const fullUrl = 'http://localhost/api/test' + query;
 
